1ab212da1ea2590f535d4ac16d34f8b4a4ca15a6
Sergio Metra <sergiop.pias@gmail.com>
2019-03-06 23:34:02 +0100

    de nuevo con bin

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const cors = require('cors')
+var app = express();
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+
+
+app.use('/team', require ('./Routes/team.js'))
+app.use('/user', require ('./Routes/users.js'))
+app.use('/role', require ('./Routes/role.js'))
+app.use('/city', require ('./Routes/city.js'))
+app.use('/idea', require ('./Routes/idea.js'))
+app.use('/auth-users', require ('./Routes/reg'))
+app.use('/authentication', require ('./Routes/regUser.js'))
+app.use('/businessmodel', require ('./Routes/businessmodel.js'))
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
